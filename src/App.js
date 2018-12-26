764269efe0705379aed18b576dac8c131deab730
KarinaDavtyan <k.davtyan27@gmail.com>
2018-12-26 23:15:30 +0100

    add emotion

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { Component} from "react";
 import "./App.css";
 import { hot } from "react-hot-loader";
+import styled from '@emotion/styled'
 
+const Button = styled.button`
+  color: hotpink;
+`
 
 class App extends Component{
   render(){
     return(
-      <div classNam e="App">
-        <h1> Hello, World! </h1>
+      <div className="App">
+        <Button>This is a hotpink button.</Button>
       </div>
     );
   }
