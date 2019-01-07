c9a1ae09fa4efbfae09f65d4cb1da04e6d52e51a
KarinaDavtyan <k.davtyan27@gmail.com>
2019-01-07 23:26:02 +0100

    Updates

--- webpack.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = (env) => {
-  if (env === 'development' || env === 'production') {
-    return require(`./webpack.${env}.config.js`);
-  } else {
-    console.warn('Wrong webpack build parameter.');
-  }
-};
